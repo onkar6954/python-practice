530d1ae491dce5dbc26d5febdba03ba20079a40e
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-04-01 20:47:28 +0530

    Add files via upload

---
+++ Practice Day 2/03_largest_of_three_no.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+if(num1>num2):
+  if(num1>num3):
+    print(f"{num1} is largest")
+  else:
+    print(f"{num3} is largest")
+else:
+  if(num2>num3):
+    print(f"{num2} is largest")
+  else:
+    print(f"{num3} is largest")
---
+++ Practice Day 2/04_positive_negative_zero.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+
+if(num>0):
+  print("It's a Positive Number")
+elif(num<0):
+  print("It's a Negative Number")
+else:
+  print("It's Zero")
