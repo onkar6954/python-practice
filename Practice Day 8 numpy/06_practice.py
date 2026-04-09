0b11f89cec5e2f6a65a9a64ecf5c8b4a6030c167
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-04-09 14:14:46 +0530

    Intermediate Array Logic

---
+++ Practice Day 8 numpy/06_practice.py
@@ -0,0 +1,9 @@
+# Find indices where elements are even.
+
+import numpy as np
+
+# arr = np.array([1, 3, 5, 8, 9, 12, 14, 15, 18, 20])
+# print(np.argwhere(arr%2==0))
+
+arr2d = np.array([[1,2,3,4,5],[6,7,8,9,0]])
+print(np.argwhere(arr2d%2==0))
