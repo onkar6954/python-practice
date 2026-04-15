4063aa71984c99b40e9a0ecd0e3bc961e353ffd6
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-04-15 14:58:05 +0530

    Fill missing values using column mean.

---
+++ Practice Day 11 pandas/03. Data Cleaning/07_practice.py
@@ -0,0 +1,7 @@
+# Fill missing values using column mean.
+
+import pandas as pd
+
+df = pd.read_csv('python-practice\Practice Day 11 pandas\sales_data_sample.csv', encoding='latin1')
+
+print(df.fillna(df.columns))
