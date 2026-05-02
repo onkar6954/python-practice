780c2ef51498cdaf2252a38de0b1624b3f533b38
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-05-02 18:20:52 +0530

    Write a Python program to do arithmetical operations addition and division.

--- Python Q Practice/practice1.py
+++ Python Q Practice/practice1.py
@@ -1,2 +1,7 @@
 """ Write a Python program to print "Hello World"."""
-print("Hello World")
+# print("Hello World")
+
+""" Write a Python program to do arithmetical operations addition and division."""
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter a number:"))
+print(num1 + num2)
