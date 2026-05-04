eb6d926bb97637790e1bc77bdc73344f9688845f
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-05-04 08:27:36 +0530

    Pattern 2

--- Python Q Practice/starPatterns.py
+++ Python Q Practice/starPatterns.py
@@ -7,7 +7,21 @@
 * * * * * 
 
 """
-for i in range(5):
-  for j in range(5):
-    print("*", end=' ')
-  print()
+# for i in range(5):
+#   for j in range(5):
+#     print("*", end=' ')
+#   print()
+
+""" Pattern 2
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+"""
+# for i in range(6):
+#   for j in range(i):
+#     print("*", end=" ")
+#   print()
