a7f29b1835f9d35d57f8e9ff3d9e394b8fc81a35
Onkar Nanna <81066990+onkar6954@users.noreply.github.com>
2026-05-04 11:12:40 +0530

    Pattern 3

--- Python Q Practice/starPatterns.py
+++ Python Q Practice/starPatterns.py
@@ -25,3 +25,17 @@
 #   for j in range(i):
 #     print("*", end=" ")
 #   print()
+
+""" Pattern 3
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+"""
+# for i in range(5):
+#   for j in range(5,i,-1):
+#     print("*",end=' ')
+#   print()
